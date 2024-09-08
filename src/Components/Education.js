1dd031f9493dcdc20097d67e231b1851cbdc844f
zil06 <zikrashamim123@gmail.com>
2024-09-08 23:02:14 +0530

    second commit

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -3,7 +3,9 @@ import React from "react";
 function Education(){
     return(
         <>
-        
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam doloremque assumenda neque cumque ea, mollitia laborum aspernatur amet iste, explicabo eligendi odit fugiat quis commodi maiores facilis obcaecati iusto officia ab exercitationem. Ullam perferendis harum similique laboriosam. Culpa accusamus, enim iste sit sapiente, autem iure perspiciatis impedit inventore magnam quos?
+        </div>
         </>
     );
 }
